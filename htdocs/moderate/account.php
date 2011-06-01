91feef607687b1262a949835cffd850cdd819846
Jesse Morgan <jesse@jesterpm.net>
2011-06-01 12:30:31 -0700

    Started the account page.

---
+++ htdocs/moderate/account.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Foursquare Community Site
+ * 
+ * Copyright (C) 2011 Foursquare Church.
+ * 
+ * Developers: Jesse Morgan <jmorgan@foursquarestaff.com>
+ *
+ */
+
+require_once('../src/base.inc.php');
+
+require_once('src/header.inc.php');
+
+echo "<h3>Your Account</h3>";
+
+
+
+
+require_once('src/footer.inc.php');
+
+?>
