3ffd7a349728e4aff80a982bb16c5196e55795e3
Jesse Morgan <jesse@jesterpm.net>
2011-06-07 16:25:09 -0700

    Removed more branding

--- htdocs/moderate/src/header.inc.php
+++ htdocs/moderate/src/header.inc.php
@@ -32,9 +32,9 @@ if (!isset($_SESSION['currentUser'])) {
         alt="<?= $CONFIG['sitetitle'] ?>" /></a></p>
 
     <div id="about">
-        Foursquare community is a place where you can find help,
+        List-and-Share is a place where you can find help,
         sell merchandise, list events or even post your rental.
-        We want to build a help you get connected to the community of our church!
+        We want to build a help you get connected to the community of the church!
     </div>
 </div>
 
