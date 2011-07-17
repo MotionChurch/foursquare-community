83255afe45c6b59ba0f6f0f0e1b9f309d18b9f6c
Jesse Morgan <jesse@jesterpm.net>
2011-07-17 07:46:19 -0700

    Adding analytics code

--- htdocs/src/footer.inc.php
+++ htdocs/src/footer.inc.php
@@ -3,6 +3,19 @@
 <div id="footer">
 Copyright &copy; 2011 listandshare.com.
 </div>
+<script type="text/javascript">
+
+ var _gaq = _gaq || [];
+ _gaq.push(['_setAccount', 'UA-24587180-1']);
+ _gaq.push(['_trackPageview']);
+
+ (function() {
+   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ })();
+
+</script>
 
 </body>
 </html>
